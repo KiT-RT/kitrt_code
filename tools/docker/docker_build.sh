c29fc97afbe4fbe21445ab04f07b1e0a9388243f
Steffen <steffen.schotthoefer@outlook.de>
2025-07-01 09:46:18 -0400

    fix a docker script

--- tools/docker/docker_build.sh
+++ tools/docker/docker_build.sh
@@ -1,2 +1,2 @@
-step 1: docker build . -t kit-rt:ML_tf
-step 2: docker run -ti --rm -v $(pwd)/../..:/mnt kit-rt:ML_tf /bin/bash  
+docker build . -t kit-rt:ML_tf
+docker run -ti --rm -v $(pwd)/../..:/mnt kit-rt:ML_tf /bin/bash
