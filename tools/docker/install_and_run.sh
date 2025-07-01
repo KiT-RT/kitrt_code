312b7cc5211e0894c245136957beb6732b726c2c
Steffen <steffen.schotthoefer@outlook.de>
2025-07-01 13:42:34 -0400

    added mesh file and install script

--- tools/docker/install_and_run.sh
+++ tools/docker/install_and_run.sh
@@ -7,6 +7,5 @@ rm -rf *
 cmake -G Ninja -DCMAKE_BUILD_UNITY=True -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=False -DBUILD_ML=True ../
 ninja
 cd ../examples
-../build_docker/KiT-RT configs/checkerboard_MN_neural.cfg
-#../build_docker/KiT-RT configs/checkerboard_MN.cfg
+../build_docker/KiT-RT configs/hohlraum_MN_neural.cfg
 cp -r /home/result /mnt/result
